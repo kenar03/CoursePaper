e8deb9110383d08985fad6440dca37f751221fe6
kenar03 <kenar592@gmail.com>
2025-04-27 21:44:31 +0500

    add usart

---
+++ source/RtosWrapper/Tasks/UsartTask.cpp
@@ -0,0 +1,22 @@
+#include "UsartTask.hpp" // for UsartTask
+
+UsartTask :: UsartTask(IDataRepositoryProvider& dataRepositoryProvider, IUsart& usart, IFormatter& formatter) :
+  mDataRepositoryProvider(dataRepositoryProvider),
+  mUsart(usart),
+  mFormatter(formatter)
+{
+}
+
+void UsartTask :: Execute()
+{
+  for(;;)
+  {
+    const auto currVoltage = mDataRepositoryProvider.GetValue();
+    const auto currFilteredVoltage = mDataRepositoryProvider.GetFilteredValue();
+    const auto voltageString = mFormatter.FormatString(currVoltage, 3U, " В", false);
+    const auto filteredVoltageString = mFormatter.FormatString(currFilteredVoltage, 3U, " В", true);
+    mUsart.SendMessage(voltageString);
+    mUsart.SendMessage(filteredVoltageString);
+    SleepUntil(500ms);
+  }
+};
